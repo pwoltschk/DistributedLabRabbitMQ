4647fceb18676696bbb855ad6804878272e41e1f
pwoltschk <p.woltschkow@gmail.com>
2017-11-03 19:23:52 +0200

    initial consumer

---
+++ MeasurementDeviceConsumer/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace MeasurementDeviceConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ MeasurementDeviceConsumer/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MeasurementDeviceConsumer")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MeasurementDeviceConsumer")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b53f36bd-3751-138e-a442-ea4629bef819")]
+
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
