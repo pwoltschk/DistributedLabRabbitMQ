92ce522a0289702aa065343b1b5dd65d7564e17b
pwoltschk <p.woltschkow@gmail.com>
2017-11-04 17:34:46 +0200

    implementation of rabbit mq

---
+++ MeasurementDeviceConsumer/RabbitMQ/RabbitMQConsumer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using RabbitMQ.Client.MessagePatterns;
+
+namespace MeasurementDeviceConsumer.RabbitMQ
+{
+    public class RabbitMQConsumer
+    {
+
+        public void CreateConnection()
+        {
+         
+        }
+
+        public void Close()
+        {
+
+        }
+
+        public void ProcessMessages()
+        {
+
+        }
+    }
+}
