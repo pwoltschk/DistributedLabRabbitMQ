05371cfd177b9a8594cd76cabe23e4b924978a9c
pwoltschk <p.woltschkow@gmail.com>
2017-11-04 16:23:23 +0200

    include rabbit mq

--- MeasurementDeviceConsumer/Program.cs
+++ MeasurementDeviceConsumer/Program.cs
@@ -1,9 +1,14 @@
-﻿namespace MeasurementDeviceConsumer
+﻿using MeasurementDeviceConsumer.RabbitMQ;
+
+namespace MeasurementDeviceConsumer
 {
     class Program
     {
         static void Main(string[] args)
         {
+            RabbitMQConsumer client = new RabbitMQConsumer();
+            client.CreateConnection();
+            client.ProcessMessages();
         }
     }
 }
