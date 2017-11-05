cc9c6c15540ff5e16a503649fd22ee4a9bab4352
pwoltschk <p.woltschkow@gmail.com>
2017-11-05 08:58:17 +0200

    include model

---
+++ MeasurementDeviceConsumer/RabbitMQ/DeviceMeasurement.cs
@@ -0,0 +1,9 @@
+﻿namespace MeasurementDeviceConsumer.RabbitMQ
+{
+    public class DeviceMeasurement
+    {
+        public decimal Value { get; set; }
+        public string DeviceNumber { get; set; }
+        public string Name { get; set; }
+    }
+}
