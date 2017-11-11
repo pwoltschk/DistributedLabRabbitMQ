fd9269f872e20ab1b08d76ab15d0c2dd532261fc
pwoltschk <p.woltschkow@gmail.com>
2017-11-11 16:43:41 +0200

    base implementation consumer

--- MeasurementDeviceConsumer/RabbitMQ/RabbitMQConsumer.cs
+++ MeasurementDeviceConsumer/RabbitMQ/RabbitMQConsumer.cs
@@ -7,20 +7,56 @@ namespace MeasurementDeviceConsumer.RabbitMQ
 {
     public class RabbitMQConsumer
     {
+        private static ConnectionFactory _factory;
+        private static IConnection _connection;
+        
+        private const string ExchangeName = "Topic_Exchange";
+        private const string DeviceMeasurementQueueName = "DeviceMeasurementTopic_Queue";
 
         public void CreateConnection()
         {
-         
+            _factory = new ConnectionFactory {
+                HostName = "localhost",
+                UserName = "guest", Password = "guest" };            
         }
 
         public void Close()
         {
-
+            throw new NotFiniteNumberException("Not implemented");
         }
 
         public void ProcessMessages()
         {
+            using (_connection = _factory.CreateConnection())
+            {
+                using (var channel = _connection.CreateModel())
+                {
+                    Console.WriteLine("Listening for Topic <measurement.devicemeasurement>");
 
+                    channel.ExchangeDeclare(ExchangeName, "topic");
+                    channel.QueueDeclare(DeviceMeasurementQueueName, 
+                        true, false, false, null);
+
+                    channel.QueueBind(DeviceMeasurementQueueName, ExchangeName, 
+                        "measurement.devicemeasurement");
+
+                    channel.BasicQos(0, 10, false);
+                    Subscription subscription = new Subscription(channel, 
+                        DeviceMeasurementQueueName, false);
+                    
+                    while (true)
+                    {
+                        BasicDeliverEventArgs deliveryArguments = subscription.Next();
+
+                        var message = 
+                            (DeviceMeasurement)deliveryArguments.Body.DeSerialize(typeof(DeviceMeasurement));
+
+                        var routingKey = deliveryArguments.RoutingKey;
+
+                        Console.WriteLine("--- Measurement - Routing Key <{0}> : {1} : {2}", routingKey, message.DeviceNumber, message.Value);
+                    }
+                }
+            }
         }
     }
 }
