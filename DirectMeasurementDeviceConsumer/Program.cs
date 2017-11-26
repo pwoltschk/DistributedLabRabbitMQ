e10813f344efa418d0aedc0b31607badf3dc786e
pwoltschk <p.woltschkow@gmail.com>
2017-11-26 15:19:21 +0200

    add direct consumer

--- DirectMeasurementDeviceConsumer/Program.cs
+++ DirectMeasurementDeviceConsumer/Program.cs
@@ -5,7 +5,10 @@ namespace MeasurementDeviceConsumer
     class Program
     {
         static void Main(string[] args)
-        {         
+        {
+            RabbitMQConsumer client = new RabbitMQConsumer();
+            client.CreateConnection();
+            client.ProcessMessages();            
         }
     }
 }
