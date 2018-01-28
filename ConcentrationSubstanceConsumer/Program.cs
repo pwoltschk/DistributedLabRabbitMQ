ada00ce54405ae3de7fd2e82a1ff07b16978cd90
pwoltschk <p.woltschkow@gmail.com>
2018-01-28 10:59:52 +0200

    implement consumer

--- ConcentrationSubstanceConsumer/Program.cs
+++ ConcentrationSubstanceConsumer/Program.cs
@@ -1,10 +1,15 @@
-﻿namespace ConcentrationSubstanceConsumer
+﻿using ConcentrationSubstanceConsumer.RabbitMQ;
+
+namespace ConcentrationSubstanceConsumer
 {
     class Program
     {
         static void Main(string[] args)
         {
-
+            RabbitMQConsumer client = new RabbitMQConsumer();
+            client.CreateConnection();
+            client.ProcessMessages();
+            client.Close();
         }
     }
 }
