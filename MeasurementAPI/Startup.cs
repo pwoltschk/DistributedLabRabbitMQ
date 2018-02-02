cd673d5a379640af970fe39b44ad5ba1200e2d9c
pwoltschk <p.woltschkow@gmail.com>
2018-02-02 16:29:13 +0200

    add measurements api

---
+++ MeasurementAPI/Startup.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Http;
+using Measurements.App_Start;
+using Owin;
+
+namespace Measurements
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder appbuilder)
+        {
+            var httpConfiguration = new HttpConfiguration();
+            WebApiConfig.Register(httpConfiguration);
+            appbuilder.UseWebApi(httpConfiguration);
+        }
+    }
+}
