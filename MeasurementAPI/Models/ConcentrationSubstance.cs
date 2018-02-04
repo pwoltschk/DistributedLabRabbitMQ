62e7a086b0a3936dfec57114da0b511dcf6ac8be
pwoltschk <p.woltschkow@gmail.com>
2018-02-04 18:13:15 +0200

    add models

---
+++ MeasurementAPI/Models/ConcentrationSubstance.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Measurements.Models
+{
+    public class ConcentrationSubstance
+    {
+        public decimal ValueToMeasure;
+        public string SubstanceNumber;
+        public string TankName;
+        public int MeasurementDelay;
+    }
+}
