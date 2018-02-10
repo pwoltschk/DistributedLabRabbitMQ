95e365f38419d001a8fa40a7ca3342be36c76c0e
pwoltschk <p.woltschkow@gmail.com>
2018-02-10 18:04:25 +0200

    add controller

---
+++ MeasurementAPI/Controllers/DirectDeviceMeasurementController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net;
+using System.Web.Http;
+using Measurements.Models;
+
+namespace Measurements.Controllers
+{
+    public class DirectDeviceMeasurementController : ApiController
+    {       
+[HttpPost]
+public IHttpActionResult MakeMeasurement([FromBody] DeviceMeasurement measurement)
+{
+
+}
+    }
+}
---
+++ MeasurementAPI/Controllers/QueueConcentrationSubstanceController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net;
+using System.Web.Http;
+using Measurements.Models;
+
+namespace Measurements.Controllers
+{
+    public class QueueConcentrationSubstanceController : ApiController
+    {       
+        [HttpPost]
+        public IHttpActionResult MakeMeasurement([FromBody] ConcentrationSubstance concentrationSubstance)
+        {
+
+        }
+    }
+}
